256e68a0dcebfe861adc7b65c50432348a9d9097
BocchiTheProg <bocchitheprog@gmail.com>
2023-09-16 17:51:51 +0300

    Added parenthesis as possible inputs, result from the start is string

--- ConverterRPN.rb
+++ ConverterRPN.rb
@@ -1,42 +1,53 @@
-result = []
+result = "output:"
 stack = []
 operators = [["+", "-"], ["x", "/"]]
+parenthesis = ["(", ")"]
 
 ARGV.each do |i|
-  unless operators[0].include? i or operators[1].include? i
-    result.push i
-  else
-    operator_priority = 1
-    if operators[1].include? i
-      operator_priority = 2
-    end
-    stack_operator_priority = 1;
-    loop do
+  unless operators[0].include? i or operators[1].include? i or parenthesis.include? i
+    result += " " + i
+  else 
+    if operators[0].include? i or operators[1].include? i
+      operator_priority = 1
+      if operators[1].include? i
+        operator_priority = 2
+      end
       stack_operator_priority = 1;
-      if operators[1].include? stack.last
-        stack_operator_priority = 2
+      loop do
+        stack_operator_priority = 1;
+        if operators[1].include? stack.last
+          stack_operator_priority = 2
+        end
+        if !stack.empty? and !parenthesis.include? stack.last and stack_operator_priority >= operator_priority
+          result += " " + stack.last
+          stack.pop
+        else
+          break
+        end
       end
-      if !stack.empty? and stack_operator_priority >= operator_priority
-        result.push stack.last
-        stack.pop
-      else
-        break
+      stack.push i
+    elsif i == parenthesis[0]
+      stack.push i
+    else 
+      loop do
+        unless stack.last == parenthesis[0]
+          result += " " + stack.last
+          stack.pop
+        else
+          stack.pop
+          break
+        end
       end
     end
-    stack.push i
   end
 end
 
 loop do
   unless stack.empty?
-    result.push stack.last
+    result += " " + stack.last
     stack.pop
   else
     break
   end
 end
-output = "output:"
-result.each do |i|
-  output += " " + i
-end
-puts output
+puts result
