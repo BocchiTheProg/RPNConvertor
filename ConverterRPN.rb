2316700136b619409f306573dbadc89190f8d99c
BocchiTheProg <bocchitheprog@gmail.com>
2023-09-16 20:11:39 +0300

    Cleaned up the code, added * as a multiplication sign (such as x)

--- ConverterRPN.rb
+++ ConverterRPN.rb
@@ -1,53 +1,55 @@
 result = "output:"
 stack = []
-operators = [["+", "-"], ["x", "/"]]
+operators = [["+", "-"], ["*", "x", "/"]]
 parenthesis = ["(", ")"]
 
 ARGV.each do |i|
-  unless operators[0].include? i or operators[1].include? i or parenthesis.include? i
+  if !operators[0].include? i and !operators[1].include? i and !parenthesis.include? i
     result += " " + i
+  elsif operators[0].include? i or operators[1].include? i
+    operator_priority = 
+      if operators[0].include? i
+        1
+      else
+        2
+      end
+    loop do
+      stack_operator_priority = 
+        if operators[0].include? stack.last
+          1
+        else 
+          2
+        end
+      if !stack.empty? and !parenthesis.include? stack.last and stack_operator_priority >= operator_priority
+        result += " " + stack.last
+        stack.pop
+      else
+        break
+      end
+    end
+    stack.push i
+  elsif i == parenthesis[0]
+    stack.push i
   else 
-    if operators[0].include? i or operators[1].include? i
-      operator_priority = 1
-      if operators[1].include? i
-        operator_priority = 2
-      end
-      stack_operator_priority = 1;
-      loop do
-        stack_operator_priority = 1;
-        if operators[1].include? stack.last
-          stack_operator_priority = 2
-        end
-        if !stack.empty? and !parenthesis.include? stack.last and stack_operator_priority >= operator_priority
-          result += " " + stack.last
-          stack.pop
-        else
-          break
-        end
-      end
-      stack.push i
-    elsif i == parenthesis[0]
-      stack.push i
-    else 
-      loop do
-        unless stack.last == parenthesis[0]
-          result += " " + stack.last
-          stack.pop
-        else
-          stack.pop
-          break
-        end
+    loop do
+      if stack.last != parenthesis[0]
+        result += " " + stack.last
+        stack.pop
+      else
+        stack.pop
+        break
       end
     end
   end
 end
 
 loop do
-  unless stack.empty?
+  if !stack.empty?
     result += " " + stack.last
     stack.pop
   else
     break
   end
 end
+
 puts result
